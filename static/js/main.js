c7da740cf597b501cb6be773e20dfa09980c78f9
Linus Lee <linus@thesephist.com>
2021-09-21 21:24:23 -0400

    perf: Various changes to page load

--- static/js/main.js
+++ static/js/main.js
@@ -1,37 +1,29 @@
 // Hydrate date and timestamps next to each update to match timestamp to the
 // visitor's current time zone.
 for (const timestampEl of document.querySelectorAll('.update-t')) {
-    console.log(timestampEl);
     const date = new Date(parseInt(timestampEl.getAttribute('data-timestamp')) * 1000);
     timestampEl.querySelector('.datestamp').textContent =
         `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`;
     timestampEl.querySelector('.datestamp').setAttribute('href',
         `/on/${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`);
     timestampEl.querySelector('.clockstamp').textContent =
-        `${date.getHours()}:${date.getMinutes().toString().padStart(2, '0')}:${date.getSeconds().toString().padStart(2, '0')}`;
+        `${date.getHours()}:${date.getMinutes().toString().padStart(2, '0')}`;
 }
 
 // Dark mode with localStorage persistence
 document.querySelector('nav').appendChild((() => {
     const b = document.createElement('button');
     b.textContent = 'light/dark';
+    b.style.color = 'var(--secondary-text)';
     b.addEventListener('click', () => {
-        document.body.classList.toggle('dark');
-        window.localStorage.setItem('colorscheme',
-            document.body.classList.contains('dark') ? 'dark' : 'light');
+        window.localStorage.setItem(
+            'colorscheme',
+            currentlyPrefersDark() ? 'light' : 'dark',
+        );
+        updateScheme();
     });
     return b;
 })());
-const savedScheme = window.localStorage.getItem('colorscheme');
-if (savedScheme != null) {
-    if (savedScheme === 'light') {
-        document.body.classList.remove('dark');
-    } else {
-        document.body.classList.add('dark');
-    }
-} else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.body.classList.add('dark');
-}
 
 // Tab key in textarea
 for (const area of document.querySelectorAll('textarea')) {
